f945c7ad507acd6a4268c1fbd1e12aac9c04dca6
fabibru <fabian@knuper.com>
2018-12-04 14:39:53 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="gnuper",
-    version="0.0.2",
+    version="0.0.3",
     author="Fabian Bruckschen",
     author_email="fabian@knuper.com",
     description="Open Source Package for Mobile Phone Metadata Preprocessing",
